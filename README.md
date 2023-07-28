29eeb3b3d0f541dca2959c7a9cfa4576dafee4b3
Felipe Rogerio <felipe.rogerio@outlook.com>
2023-07-28 00:36:58 -0300

    Versão final do site que foi tirado do ar após um mês da festa.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # formatura
+
+#Site Criado para envio junto ao convite de formatura de 6 formandas. 
+
+A confirmação de presença é foi feita através do site e fpoi criado uma pagina (lista.html) para checar as respostas.
+
+Utilizei um arquivo json pois as formandas não desejaram gastar com banco de dados.
+
+os nomes no arquivo lista.json foram gerados aleatoriamente para ficar aqui de exemplo.
--- Script/relogio.js
+++ Script/relogio.js
@@ -9,18 +9,34 @@ function atualizarRelogio() {
   // Calcula o tempo restante até o evento
   const tempoRestante = dataEvento.getTime() - dataAtual.getTime();
   
-  // Calcula os dias, horas e minutos restantes
-  const dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
-  const horas = Math.floor((tempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60));
-  
-  // Atualiza os elementos HTML com os novos valores
-  document.getElementById('dia1').innerHTML = Math.floor(dias / 10);
-  document.getElementById('dia2').innerHTML = dias % 10;
-  document.getElementById('hora1').innerHTML = Math.floor(horas / 10);
-  document.getElementById('hora2').innerHTML = horas % 10;
-  document.getElementById('minuto1').innerHTML = Math.floor(minutos / 10);
-  document.getElementById('minuto2').innerHTML = minutos % 10;
+  if (tempoRestante < 0){
+    // Atualiza os elementos HTML com os novos valores
+    document.getElementById('dia1').innerHTML = 0;
+    document.getElementById('dia2').innerHTML = 0;
+    document.getElementById('hora1').innerHTML = 0;
+    document.getElementById('hora2').innerHTML = 0;
+    document.getElementById('minuto1').innerHTML = 0;
+    document.getElementById('minuto2').innerHTML = 0;
+
+    document.querySelector('.comemorarTitulo').innerHTML = 'NOSSA FESTA FOI LINDA!'
+
+  }
+  else{
+    
+    // Calcula os dias, horas e minutos restantes
+    const dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((tempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+    
+    // Atualiza os elementos HTML com os novos valores
+    document.getElementById('dia1').innerHTML = Math.floor(dias / 10);
+    document.getElementById('dia2').innerHTML = dias % 10;
+    document.getElementById('hora1').innerHTML = Math.floor(horas / 10);
+    document.getElementById('hora2').innerHTML = horas % 10;
+    document.getElementById('minuto1').innerHTML = Math.floor(minutos / 10);
+    document.getElementById('minuto2').innerHTML = minutos % 10;
+  }
+
 }
 
 // Atualiza o relógio a cada segundo
